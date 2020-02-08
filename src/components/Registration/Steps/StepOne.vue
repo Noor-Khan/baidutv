f3ec0a9e7e78290764f519f5c2f8f5e907c75a6a
noor-khan <foruconnect@gmail.com>
2020-02-08 12:24:51 +0500

    remove passworld and password confirm module

--- src/components/Registration/Steps/StepOne.vue
+++ src/components/Registration/Steps/StepOne.vue
@@ -11,45 +11,7 @@
         ></v-text-field>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="12" md="8" offset-md="2" sm="12">
-        <v-text-field
-          label="Password"
-          hide-details="auto"
-          v-model="password"
-          :rules="passwordRules"
-          type="password"
-          required
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="auto" md="8" offset-md="2">
-        <v-text-field
-          label="Confirm Password"
-          hide-details="auto"
-          v-model="confirmPassword"
-          :rules="[...confirmPasswordRules, passwordConfirmationRule]"
-          type="password"
-        ></v-text-field>
-        <div class="legal pt-4">
-          <v-checkbox
-            v-model="terms"
-            :rules="[v => !!v || 'You must agree to continue!']"
-            class="mx-2 mt-0"
-            color="primary"
-          >
-            <template v-slot:label>
-              <div>
-                By clicking you agree to be
-                <a href="/legal" target="_blank">Terms & Conditions</a>
-                to use.
-              </div>
-            </template>
-          </v-checkbox>
-        </div>
-      </v-col>
-    </v-row>
+    <v-row></v-row>
     <v-row>
       <v-col cols="12">
         <primary-button text="Register" @click.native="validate" btnClass="primary"></primary-button>
@@ -79,24 +41,12 @@ export default {
       snackbar: false,
       lazy: false,
       email: "",
-      password: "",
-      confirmPassword: "",
       terms: null,
       emailRules: [
         v => !!v || "E-mail is required",
         v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-      ],
-      passwordRules: [
-        v => !!v || "Password is required",
-        v => (v && v.length >= 5) || "Password must have 5+ characters"
-      ],
-      confirmPasswordRules: [v => !!v || "Password is required"]
+      ]
     };
-  },
-  computed: {
-    passwordConfirmationRule() {
-      return this.password === this.confirmPassword || "Password must match";
-    }
   },
   methods: {
     validate() {
